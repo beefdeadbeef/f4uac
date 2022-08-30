636e9fa7ce8487253691efde73cd509806cb1fd2
Sergey Bolshakov <beefdeadbeef@gmail.com>
2022-08-30 14:58:52 +0300

    pwm: use break machinery

--- pwm.c
+++ pwm.c
@@ -23,9 +23,6 @@ void *pframe(frame_type frame)
 
 static void timer_tim1_setup_ocs(enum tim_oc_id oc, enum tim_oc_id ocn)
 {
-	timer_disable_oc_output(TIM1, oc);
-	timer_disable_oc_output(TIM1, ocn);
-
 	timer_disable_oc_clear(TIM1, oc);
 	timer_enable_oc_preload(TIM1, oc);
 	timer_set_oc_slow_mode(TIM1, oc);
@@ -63,9 +60,6 @@ void pwm()
 	timer_set_enabled_off_state_in_idle_mode(TIM1);
 	timer_set_enabled_off_state_in_run_mode(TIM1);
 	timer_disable_break(TIM1);
-	timer_set_break_polarity_high(TIM1);
-	timer_disable_break_automatic_output(TIM1);
-	timer_set_break_lock(TIM1, TIM_BDTR_LOCK_OFF);
 	timer_tim1_setup_ocs(TIM_OC1, TIM_OC1N);
 	timer_tim1_setup_ocs(TIM_OC2, TIM_OC2N);
 	timer_enable_preload(TIM1);
@@ -73,11 +67,11 @@ void pwm()
 	TIM_DCR(TIM1) |= 1<<8 | 0xd;
 	timer_generate_event(TIM1, TIM_EGR_UG);
 	timer_enable_irq(TIM1, TIM_DIER_UDE);
-	timer_enable_break_main_output(TIM1);
 }
 
 void pwm_enable(void)
 {
+	timer_enable_break_main_output(TIM1);
 	timer_enable_counter(TIM1);
 	gpio_clear(GPIOB, GPIO12);
 }
@@ -86,6 +80,7 @@ static void pwm_disable(void)
 {
 	gpio_set(GPIOB, GPIO12);
 	timer_disable_counter(TIM1);
+	timer_disable_break_main_output(TIM1);
 }
 
 extern volatile ev_t e;
